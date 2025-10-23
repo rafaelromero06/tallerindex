4429949133d9f736a4d09fde250b8a6632668973
rafaelromero06 <arafaelr@uninorte.edu.co>
2025-10-23 21:12:32 +0000

    h

---
+++ persona.py
@@ -0,0 +1,7 @@
+class persona:
+    def __init__ (self,name,lastname,phone):
+        self.name = name
+        self.lastname = lastname
+        self.phone = phone
+        self.isActive = isActive
+        
